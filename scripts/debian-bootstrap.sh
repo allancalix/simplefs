921c92c4c6993b40134c374468eabdfd5b54fccb
Allan Calix <contact@allancalix.com>
2020-05-15 22:46:24 -0700

    Add fuse to debian vagrant setup
    Fuse itself is required as a runtime dependency to bind against for the
    libraries.

--- scripts/debian-bootstrap.sh
+++ scripts/debian-bootstrap.sh
@@ -3,5 +3,5 @@
 set -e
 
 apt-get -y update && apt-get -y install \
-  libfuse-dev pkg-config gcc llvm libclang-dev clang curl
+  fuse libfuse-dev pkg-config gcc llvm libclang-dev clang curl
 su vagrant -c "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y"
